ca22d097ef1c85a0d26c09c0c3060180c9ec4a00
B-nod <binodtamang245@gmail.com>
2024-04-01 19:58:47 +0545

    itemloader

--- ebook_scraper/items.py
+++ ebook_scraper/items.py
@@ -3,10 +3,16 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
+from itemloaders.processors import MapCompose, TakeFirst
 
+def get_price(txt):
+    return float(txt.replace('£',''))
 
-class EbookScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class EbookItem(Item):
+    
+    title = Field()
+    price = Field(
+        input_processor=MapCompose(get_price),
+        output_processor=TakeFirst()
+    )
--- ebook_scraper/spiders/ebook.py
+++ ebook_scraper/spiders/ebook.py
@@ -1,20 +1,24 @@
 import scrapy
+from ebook_scraper.items import EbookItem
+from scrapy.loader import ItemLoader
+
+
 
 class EbookSpider(scrapy.Spider):
     name = "ebook"
 
-    start_urls = ['https://books.toscrape.com/']
+    start_urls = ['https://books.toscrape.com/catalogue/category/books/travel_2/']
 
     def parse(self, response):
         print("[ parse ]")
         # print(response.css("h3 a::text").get())
 
-        ebooks = response.css("article")
+        ebooks = response.css("article.product_pod")
         for ebook in ebooks:
-            title = ebook.css("a::text").get()
-            price = ebook.css("p.price_color::text").get()
+            loader = ItemLoader(item=EbookItem(), selector=ebook)
+            loader.add_css('title', "h3 a::attr(title)")
+            # loader.add_value('price', ebook.css("p.price_color::text").get())
+            loader.add_css('price', "p.price_color::text")
             
-            yield{
-                "title":title,
-                "price":price
-            }
+            
+            yield loader.load_item()
